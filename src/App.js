84ec38b3ff36a45006e1f9d18dfda03adbb091a6
nizamy710 <nizamy710@gamil.com>
2021-04-17 11:20:15 +0530

    neogcamp-emoji-meaning-finder
    reactjs app to find the emoji meaning

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const emojiDictionary = {
+  "😀": "Grinning Face",
+  "😃": "Grinning Face with Big Eyes",
+  "😄": "Grinning Face with Smiling Eyes",
+  "😁": "Beaming Face with Smiling Eyes",
+  "😆": "Grinning Squinting Face",
+  "😅": "Grinning Face with Sweat"
+};
+
+var emojiWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setEmojiMeaning] = useState("insert emoji to check meaning");
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "We don't have this in our database";
+    }
+    setEmojiMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setEmojiMeaning(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1>Inside Out</h1>
+      <input onChange={emojiInputHandler} />
+      <div style={{ padding: "0.5rem" }}>{meaning}</div>
+
+      <h3>click on the emoji to check the meaning </h3>
+
+      {emojiWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
